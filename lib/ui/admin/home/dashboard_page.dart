0a23c7ad091dda986b232985c8b6afe277f9eadf
yash <dharvaparamaryash@gmail.com>
2025-06-20 17:42:33 +0530

    Admin Dashboard_page created

--- lib/ui/admin/home/dashboard_page.dart
+++ lib/ui/admin/home/dashboard_page.dart
@@ -1,10 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:motor_vehicle/widgets/container_table.dart';
 
 class Dashboard_page extends StatelessWidget {
   const Dashboard_page({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+        title: Text('Dashboard',
+            style: TextStyle(color: Colors.white),
+        ),
+        
+      ),
+      body: ListView(
+
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(30),bottomRight: Radius.circular(30)
+                ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: ListTile(
+                title: Text('BL Kumawat',style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),),
+                subtitle: Text('Welcome Back',style: TextStyle(color: Colors.white,fontSize: 14,fontWeight: FontWeight.bold),),
+                trailing: CircleAvatar(backgroundImage: AssetImage('assets/images/person.jpg'),radius: 30,),
+              ),
+            ),
+          ),
+
+          GridView.count(
+            shrinkWrap: true,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+            padding: EdgeInsets.all(30),
+            crossAxisCount: 2,
+            children: [
+              Container_table(
+                tital: "Customer",
+                u_icon: Icons.people,
+                bg_color: Colors.lightBlue,
+              ),
+              Container_table(
+                tital: "Booking",
+                u_icon: Icons.bookmark_add,
+                bg_color: Colors.amber,
+              ),
+              Container_table(
+                tital: "Driver",
+                u_icon: Icons.emoji_people,
+                bg_color: Colors.purple,
+              ),
+              Container_table(
+                tital: "Package",
+                u_icon: Icons.paste_outlined,
+                bg_color: Colors.lightGreen,
+              ),
+              Container_table(
+                tital: "Vehicle",
+                u_icon: Icons.drive_eta,
+                bg_color: Color.fromARGB(255, 218, 67, 40),
+              ),
+              Container_table(
+                tital: "Attendance",
+                u_icon: Icons.add_task,
+                bg_color: Color.fromARGB(255, 26, 180, 216),
+              ),
+            ],
+            ),
+        ],
+      ),
+    );
   }
 }
--- lib/ui/splash_page.dart
+++ lib/ui/splash_page.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:motor_vehicle/ui/admin/home/dashboard_page.dart';
 import 'package:motor_vehicle/ui/login_page.dart';
 
 class SplashPage extends StatefulWidget {
@@ -15,9 +16,13 @@ class _SplashPageState extends State<SplashPage> {
   void initState() {
     super.initState();
 
+    // Timer(
+    //   Duration(seconds: 5),
+    //   () => Get.off(LoginPage()),
+    // );
     Timer(
-      Duration(seconds: 5),
-      () => Get.off(LoginPage()),
+      Duration(seconds: 1),
+      () => Get.off(Dashboard_page()),
     );
   }
 
